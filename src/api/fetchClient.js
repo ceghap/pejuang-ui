460ef3475686cf9f097ff5fdbfac1ba67e28b15e
Ashraf Latif <ohashraf@gmail.com>
2026-04-12 11:11:32 +0800

    prepare for vercel

--- src/api/fetchClient.js
+++ src/api/fetchClient.js
@@ -1,6 +1,6 @@
 import { useAuthStore } from '../store/authStore';
 
-const BASE_URL = 'http://localhost:5000/api';
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api';
 
 /**
  * Custom fetch wrapper that automatically:
